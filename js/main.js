4efd555c335f09ed5fbff66deb0b18eb89228e77
anapayanruiz <anapayan22@gmail.com>
2019-11-20 09:41:20 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const button = document.querySelector('.js-button');
 const inputNumber = document.querySelector('.js-input-number');
 
 //variables para añadir o quitar frases
+const inputNumberStop = document.querySelector('.js-result-stop');
 const inputNumberStopHigh = document.querySelector('.js-result-stop-high');
 const inputNumberStopLow = document.querySelector('.js-result-stop-low');
 const inputNumberGo = document.querySelector('.js-result-go');
@@ -23,20 +24,24 @@ const randomNumber = getRandomNumber(100);
 console.log(`Me genera un numero aleatorio ${randomNumber}`);
 
 
+//funcion para enlazar la funcion 
 
 
 
 //funcion para comprobar si el numero es igual al que ponemos
 //en el input
-function comparateRandomNumber(randomNumber) {
-    const inputNumberValue = inputNumber.value;
+function comparateRandomNumber() {
+    const inputNumberValue = parseInt(inputNumber.value);
+
     if (inputNumberValue === randomNumber) {
         inputNumberGo.classList.remove('hidden');
     } else if (inputNumberValue > randomNumber) {
         inputNumberStopHigh.classList.remove('hidden');
 
-    } else {
+    } else if (inputNumberValue < randomNumber) {
         inputNumberStopLow.classList.remove('hidden');
+    } else {
+        inputNumberStop.classList.remove('hidden');
     }
 };
 
